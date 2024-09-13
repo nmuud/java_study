82e6a7d5e871bb5984edade34f852c2ccc276bfd
nmuud <nmuud@naver.com>
2024-09-13 14:09:49 +0900

    format 함수로 변경

--- src/zero/JavaStudy01.java
+++ src/zero/JavaStudy01.java
@@ -8,7 +8,7 @@ public class JavaStudy01 {
 
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= 9; j++) {
-                System.out.printf("%02d x %02d = %02d    ", j, i, j * i);
+                System.out.print(String.format("%02d x %02d = %02d\t", j, i, j * i));
             }
 
             System.out.println();
